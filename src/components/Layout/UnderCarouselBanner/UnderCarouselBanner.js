5e0b35f27e2df5a2f2de0fe2559786b9875514e8
Hassan <hassanbilalchannel@gmail.com>
2022-01-28 13:09:19 +0500

    first commit

---
+++ src/components/Layout/UnderCarouselBanner/UnderCarouselBanner.js
@@ -0,0 +1,11 @@
+import UnderCarouselBannerImg from '../../../assets/banners/underCarouselBanner.jpg';
+
+import classes from './UnderCarouselBanner.module.css';
+
+export const UnderCarouselBanner = () => {
+  return (
+    <div className={classes['under-carousel-banner-container']}>
+      <img src={UnderCarouselBannerImg} />
+    </div>
+  );
+};
