1bd625cd8e1e044ffee89dbc6882af3dbb39b499
Ali Raza <raza80540@gmail.com>
2022-02-25 17:17:09 +0500

    cookies accept add

--- src/cookies/NewLetter.js
+++ src/cookies/NewLetter.js
@@ -1,15 +1,20 @@
 /** @format */
 import CookieConsent from "react-cookie-consent";
-import "../cookies/cookie.css";
 
 const NewsLetter = () => {
   return (
     <div>
       <CookieConsent
-        className="cookieBg"
         location="bottom"
         buttonText="Accept!"
         cookieName="userExperience"
+        style={{ background: "black" }}
+        buttonStyle={{
+          color: "white",
+          fontSize: "14px",
+          fontWeight: "bold",
+          background: "#3b6d3a",
+        }}
         expires={150}
       >
         This website uses cookies to enhance the user experience
