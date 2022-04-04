453c3f4bbe0e1f33cfc7ce4f5e519ba086e942a8
Ali Raza <raza80540@gmail.com>
2022-04-04 12:16:06 +0500

    color chnage

--- src/services/productData.js
+++ src/services/productData.js
@@ -47,11 +47,11 @@ import shopProductCategoryGiftsAndValueSetsImg from "../assets/shop/ProductCateg
 
 // Import imges for Shop Procuct Company Categories
 
-import proteinCompanyProductsTheProteinWorks from "../assets/shop/companiesImgs/theProteinWorks.jpg";
-import proteinCompanyProductsCellulor from "../assets/shop/companiesImgs/cellular.jpg";
-import proteinCompanyProductsMuscleTech from "../assets/shop/companiesImgs/muscleTech.jpg";
-import proteinCompanyProductsOn from "../assets/shop/companiesImgs/on.jpg";
-import proteinCompanyProductsJackedNutrition from "../assets/shop/companiesImgs/jackNeutrition.jpg";
+// import proteinCompanyProductsTheProteinWorks from "../assets/shop/companiesImgs/theProteinWorks.jpg";
+// import proteinCompanyProductsCellulor from "../assets/shop/companiesImgs/cellular.jpg";
+// import proteinCompanyProductsMuscleTech from "../assets/shop/companiesImgs/muscleTech.jpg";
+// import proteinCompanyProductsOn from "../assets/shop/companiesImgs/on.jpg";
+// import proteinCompanyProductsJackedNutrition from "../assets/shop/companiesImgs/jackNeutrition.jpg";
 
 //------------------Product Data By Category------------------
 
@@ -102,12 +102,12 @@ export const DUMMY_PRODUCTS = [
     price: "Rs 12,000",
     rating: "4.5",
   },
-  { 
+  {
     id: 7,
-    name: "Name", 
-    img: imgSeven, 
-    price: "Rs 12,000", 
-    rating: "4.5" 
+    name: "Name",
+    img: imgSeven,
+    price: "Rs 12,000",
+    rating: "4.5",
   },
   {
     id: 8,
@@ -116,12 +116,13 @@ export const DUMMY_PRODUCTS = [
     price: "Rs 12,000",
     rating: "4.5",
   },
-  { 
+  {
     id: 9,
-    name: "Name", 
-    img: imgNine, 
-    price: "Rs 12,000", 
-    rating: "4.5" },
+    name: "Name",
+    img: imgNine,
+    price: "Rs 12,000",
+    rating: "4.5",
+  },
   {
     id: 10,
     name: "Name",
@@ -129,12 +130,13 @@ export const DUMMY_PRODUCTS = [
     price: "Rs 12,000",
     rating: "4.5",
   },
-  { 
+  {
     id: 11,
-    name: "Name", 
-    img: imgEleven, 
-    price: "Rs 12,000", 
-    rating: "4.5" },
+    name: "Name",
+    img: imgEleven,
+    price: "Rs 12,000",
+    rating: "4.5",
+  },
   {
     id: 12,
     name: "Name",
@@ -233,12 +235,12 @@ export const shopProductCategories = [
 
 //--------------------------------SHOP PRODUCT COMPANIES CATEGORIES------------------------------
 
-export const proteinCompaniesCategory = [
-  { name: "the-protein-works", img: proteinCompanyProductsTheProteinWorks },
-  { name: "cellulor", img: proteinCompanyProductsCellulor },
-  { name: "muscle-Tech", img: proteinCompanyProductsMuscleTech },
-  { name: "on", img: proteinCompanyProductsOn },
-  { name: "jack-nutrition", img: proteinCompanyProductsJackedNutrition },
-];
+// export const proteinCompaniesCategory = [
+//   { name: "the-protein-works", img: proteinCompanyProductsTheProteinWorks },
+//   { name: "cellulor", img: proteinCompanyProductsCellulor },
+//   { name: "muscle-Tech", img: proteinCompanyProductsMuscleTech },
+//   { name: "on", img: proteinCompanyProductsOn },
+//   { name: "jack-nutrition", img: proteinCompanyProductsJackedNutrition },
+// ];
 
 // rafce shortcut
