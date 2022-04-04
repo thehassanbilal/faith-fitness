544cacb1d1481f48069cc9b276df77a2b0f0bd34
Ali Raza <raza80540@gmail.com>
2022-04-04 15:10:42 +0500

    color chnage

--- src/components/Layout/OurPrograms/OurPrograms.js
+++ src/components/Layout/OurPrograms/OurPrograms.js
@@ -1,44 +1,45 @@
 /** @format */
 
-import OurProgramsCard from '../../Cards/OurProgramsCard/OurProgramsCard';
-import classes from './OurPrograms.module.css';
-import icons1 from '../../../assets/icons-1.png';
-import icons2 from '../../../assets/icons-2.png';
-import icons3 from '../../../assets/icons-3.png';
-import icons4 from '../../../assets/icons-4.png';
+import OurProgramsCard from "../../Cards/OurProgramsCard/OurProgramsCard";
+import classes from "./OurPrograms.module.css";
+import icons1 from "../../../assets/icons-1.png";
+import icons2 from "../../../assets/icons-2.png";
+import icons3 from "../../../assets/icons-3.png";
+import icons4 from "../../../assets/icons-4.png";
+import "../../../App.css";
 
 const OurPrograms = () => {
-  const learnMore = 'Learn More..';
+  const learnMore = "Learn More..";
   const programsData = [
     {
-      logo: <img src={icons1} height='80' width='80' />,
-      title: 'Flex Muscle',
-      desc: 'Lacking motivation to get back into the gym? Back in the gym already but feel stagnated? Or maybe you’re just looking for new inspiration for a the new year. Whatever your situation, reading words of wisdom from successful people can be super-inspiring.',
+      logo: <img src={icons1} height="80" width="80" />,
+      title: "Flex Muscle",
+      desc: "Lacking motivation to get back into the gym? Back in the gym already but feel stagnated? Or maybe you’re just looking for new inspiration for a the new year. Whatever your situation, reading words of wisdom from successful people can be super-inspiring.",
     },
     {
-      logo: <img src={icons4} height='80' width='80' />,
-      title: 'Cardio Exercises',
-      desc: 'Lacking motivation to get back into the gym? Back in the gym already but feel stagnated? Or maybe you’re just looking for new inspiration for a the new year. Whatever your situation, reading words of wisdom from successful people can be super-inspiring.',
+      logo: <img src={icons4} height="80" width="80" />,
+      title: "Cardio Exercises",
+      desc: "Lacking motivation to get back into the gym? Back in the gym already but feel stagnated? Or maybe you’re just looking for new inspiration for a the new year. Whatever your situation, reading words of wisdom from successful people can be super-inspiring.",
     },
     {
-      logo: <img src={icons2} height='80' width='80' />,
-      title: 'Body Weight',
-      desc: 'Lacking motivation to get back into the gym? Back in the gym already but feel stagnated? Or maybe you’re just looking for new inspiration for a the new year. Whatever your situation, reading words of wisdom from successful people can be super-inspiring.',
+      logo: <img src={icons2} height="80" width="80" />,
+      title: "Body Weight",
+      desc: "Lacking motivation to get back into the gym? Back in the gym already but feel stagnated? Or maybe you’re just looking for new inspiration for a the new year. Whatever your situation, reading words of wisdom from successful people can be super-inspiring.",
     },
     {
-      logo: <img src={icons3} height='80' width='80' />,
-      title: 'Weight Lifting',
-      desc: 'Lacking motivation to get back into the gym? Back in the gym already but feel stagnated? Or maybe you’re just looking for new inspiration for a the new year. Whatever your situation, reading words of wisdom from successful people can be super-inspiring.',
+      logo: <img src={icons3} height="80" width="80" />,
+      title: "Weight Lifting",
+      desc: "Lacking motivation to get back into the gym? Back in the gym already but feel stagnated? Or maybe you’re just looking for new inspiration for a the new year. Whatever your situation, reading words of wisdom from successful people can be super-inspiring.",
     },
   ];
   return (
-    <section className={classes['programSection-container']}>
-      <h1 className={classes['programSection-heading']}>Our Program</h1>
-      <div className={classes['programSection-title']}>
-        <p className={classes['programSection-title-firstHalf']}>BUILD YOUR</p>
-        <p className={classes['programSection-title-secondHalf']}>BEST BODY</p>
+    <section className={classes["programSection-container"]}>
+      <h1 className={classes["programSection-heading"]}>Our Program</h1>
+      <div className={classes["programSection-title"]}>
+        <p className={classes["programSection-title-firstHalf"]}>BUILD YOUR</p>
+        <p className={classes["programSection-title-secondHalf"]}>BEST BODY</p>
       </div>
-      <div className={classes['programSection-cards']}>
+      <div className={classes["programSection-cards"]}>
         {programsData.map((cardData) => {
           return (
             <OurProgramsCard
